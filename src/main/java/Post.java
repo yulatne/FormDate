3dc45394da17bddaccd8c8f40b4a343768e3689b
yulatne <tyzhina@icloud.com>
2022-08-20 20:19:01 +0300

    comm  Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    boolean subscription;
+    public FormDate birthday;
+}
